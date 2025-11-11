3dad19efb5b8e9b6039710857cac7f126dc4d916
Kinga <278428@student.pwr.edu.pl>
2025-11-11 18:58:32 +0100

    Completeted teh getWorkoutZone method and covered all zones with tests

--- src/test/java/HRCalculatorTest.java
+++ src/test/java/HRCalculatorTest.java
@@ -22,6 +22,40 @@ public class HRCalculatorTest {
                 () -> HRCalculator.calculateMaxHR(-1));
     }
 
+    @Test
+    public void testZoneRecovery(){
+        assertEquals("Recovery", HRCalculator.getWorkoutZone(40, 100)); // 100/180=0.55
+    }
+
+    @Test
+    public void testZoneFatBurn(){
+        assertEquals("Fat burn", HRCalculator.getWorkoutZone(40, 115)); // ~0.64
+    }
+
+    @Test
+    public void testZoneCardio(){
+        assertEquals("Cardio", HRCalculator.getWorkoutZone(40, 130)); // ~0.72
+    }
+
+    @Test
+    public void testZoneAnaerobic(){
+        assertEquals("Anaerobic", HRCalculator.getWorkoutZone(40, 150)); // ~0.83
+    }
+
+    @Test
+    public void testZoneVO2(){
+        assertEquals("VO2 max", HRCalculator.getWorkoutZone(40, 172)); // ~0.95
+    }
+
+    @Test
+    public void testBpmTooHigh(){
+        assertThrows(IllegalArgumentException.class,
+                () -> HRCalculator.getWorkoutZone(40, 999));
+    }
+
+    @Test
+    public void testBpmNegative(){
+        assertThrows(IllegalArgumentException.class,
+                () -> HRCalculator.getWorkoutZone(40, -5));
+    }
 }
-
-
