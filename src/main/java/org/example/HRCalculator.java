6a8d56ebeb73e49c43a2af494562af4aad96f137
Kinga <278428@student.pwr.edu.pl>
2025-11-11 18:18:26 +0100

    Fixed the bug by adding a condidtion that age < 0

--- src/main/java/org/example/HRCalculator.java
+++ src/main/java/org/example/HRCalculator.java
@@ -2,10 +2,8 @@ package org.example;
 
 public class HRCalculator {
     public static int calculateMaxHR(int age){
-        if(age>120){
-            throw new IllegalArgumentException(
-                    "Age out of bounds"
-            );
+        if (age < 0 || age > 120) {
+            throw new IllegalArgumentException("Age must be between 0 and 120");
         }
         return 220 - age;
     }
